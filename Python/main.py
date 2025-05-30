b5fafbfd0d7728d4c1572efe8c2a8b1397be35c8
l3tox <lukaszavadil@proton.me>
2025-05-30 07:15:59 +0200

    change

--- Python/main.py
+++ Python/main.py
@@ -7,14 +7,17 @@ def main():
     parser = argparse.ArgumentParser(description="Network scanner")
     parser.add_argument("-ip", "--ip_range", help="Target IP address", required=True)
     parser.add_argument("-sS", "--syn_scan", help="Enable SYN scan", action="store_true")
+    parser.add_argument("-pS", "--ping_scan", help="ICMP ping scan", action="store_true")
     args = parser.parse_args()
 
     Scanner = NetScanner(args.ip_range)
     if Scanner.validateIPRange():
         if args.syn_scan:
             Scanner.synScan()
+        elif args.ping_scan:
+            Scanner.pingScan()
         else:
-            Scanner.arpScanner()
+            Scanner.arpScan()
 
 
     
--- Python/net.py
+++ Python/net.py
@@ -1,13 +1,15 @@
 import scapy.all as scapy
 import re
 import socket
+import time
 
 class NetScanner:
     def __init__(self, ip_range):
         self.ip_range = ip_range
-        self.clients
+        self.clients = []
 
-    def arpScanner(self):
+    def arpScan(self):
+        scapy.conf.verb = 0
         arp = scapy.ARP(pdst=self.ip_range)
         ether = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
         packet = ether/arp
@@ -15,28 +17,49 @@ class NetScanner:
         res = scapy.srp(packet, timeout=1)[0]
         clients = []
         for sent, recived in res:
-            clients.append({'ip': recived.psrc, 'mac': recived.hwsrc})
+            self.clients.append(ip=recived.psrc, mac=recived.hwsrc)
+        self.printProcess(head="ARP scan")
         
-        print("Available devices in the network: ")
-        print("IP" + " "*18+"MAC")
-        print("-----------------------------------")
-        for client in clients:
-            print("{:16}      {}".format(client['ip'], client['mac']))
-    
-    def synScan():
-        return True
+    def pingScan(self):
+        head = "Ping scan"
+        clients = []
+        scapy.conf.verb = 0
+        if "/" in self.ip_range:
+            idk = 0
+        else:
+            pkt = scapy.IP(dst=self.ip_range)/scapy.ICMP()
+            reply = scapy.sr1(pkt, timeout=1)
+            if reply:
+                self.addToClients(ip=self.ip_range)
+                self.printProcess(head=head)
+            else:
+                self.printProcess(error="Host is not alive", head=head)
 
+    # def synScan(self):
+    #     return True
+    
+    def printProcess(self, head="", error=""):
+        print(f"Results of {head}: \n")
+       
+        if error:
+            print(f"Error: {error}")
+        else:
+            print("IP" + " "*18+"MAC")
+            print("-----------------------------------")
+            for client in self.clients:
+                print("{:16}      {}".format(client['ip'], client['mac']))
+    
     def validateIPRange(self):
-        regex = "^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])/(3[0-2]|[12]?[0-9]|0)$"
+        regex = r"^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}" \
+                r"(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])" \
+                r"(\/(3[0-2]|[12]?[0-9]|0))?$"        
         match = re.search(regex, self.ip_range)
         if(match):
             return True
         else:
             return False
-    
-    def printResults(self):
-        return self.clients
-    
+    def addToClients(self, ip="0.0.0.0", mac="None"):
+        self.clients.append({'ip':ip, 'mac':mac})
     @staticmethod
     def getLocalAddress():
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create UDP socket AF_NET = IPv4, SOCK_DGRAM = UDP - SOCK_STREAM = TCP
